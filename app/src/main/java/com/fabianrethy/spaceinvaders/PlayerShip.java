d2523b36c5daf4a3160737fa0203b2507bba9071
Fábián Gábor <gabor_fabian1@epam.com>
2021-11-21 15:14:17 +0100

    PlayerShip class

--- app/src/main/java/com/fabianrethy/spaceinvaders/PlayerShip.java
+++ app/src/main/java/com/fabianrethy/spaceinvaders/PlayerShip.java
@@ -1,4 +1,27 @@
 package com.fabianrethy.spaceinvaders;
 
-public class PlayerShip {
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.RectF;
+
+public class PlayerShip extends SpaceShip {
+
+	public PlayerShip(Context context, int screenX, int screenY) {
+		rect = new RectF();
+
+		length = screenX / 10;
+		height = screenY / 10;
+		x = screenX / 2;
+		y = screenY - 20;
+		bitmap1 = BitmapFactory.decodeResource(context.getResources(), R.drawable.playership);
+		bitmap1 = Bitmap.createScaledBitmap(bitmap1, (int) (length), (int) (height), false);
+		shipSpeed = 350;
+		shipMoving = STOPPED;
+	}
+
+	public void setMovementState(int state) {
+		shipMoving = state;
+	}
 }
+
